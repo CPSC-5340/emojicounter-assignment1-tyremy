0fade9ab76ebe761738a7d4a37719881adea18bb
tyremy <101478630+tyremy@users.noreply.github.com>
2024-03-17 12:15:11 -0400

    Completed

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -7,20 +7,69 @@
 
 import SwiftUI
 
+let emojis = ["🤣", "🥳", "😡", "🥶", "👻", "👽" ]
+
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(Array(emojis.enumerated()), id: \.element) {
+                    index, emoji in
+                        emojiItem(counter: 0, image: emoji)
+                }
+            }
+            .navigationTitle("Emoji Counter")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+            ContentView()
     }
+}
+
+struct emojiItem: View {
+    
+    @State var counter = 0
+    var image : String
+    
+    var body: some View {
+        HStack {
+            //Image(systemName: "\(image)")
+                //.imageScale(.large)
+            Text("\(image)")
+                .font(.system(size: 36))
+            Text("Counter: \(counter)")
+            Spacer()
+            Image(systemName: "plus")
+                .imageScale(.large)
+                .padding(3)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(.red, lineWidth: 2)
+                )
+                .onTapGesture {
+                    counter += 1
+                }
+            Image(systemName: "minus")
+                .imageScale(.large)
+                .padding(9)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(.red, lineWidth: 2)
+                )
+                .onTapGesture {
+                    counter -= 1
+                }
+        }
+        .foregroundColor(.red)
+        .padding()
+    }
+    
 }
